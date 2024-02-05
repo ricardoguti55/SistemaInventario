36a10c1821248231a361a8473918a4aa11614f3a
ricardoguti55 <ricardoguti55@gmail.com>
2024-02-05 15:16:31 -0500

    Email

--- SistemaInventario/obj/Debug/net8.0/SistemaInventario.AssemblyInfo.cs
+++ SistemaInventario/obj/Debug/net8.0/SistemaInventario.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaInventario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a1b3e1fbfae93fe7e34f096a6e64de33ed17bb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73729f4edf0c3bcf776cce5a65fe62ea06849f77")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaInventario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaInventario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
